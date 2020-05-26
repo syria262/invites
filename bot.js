6d94fcb5d746918c8438673214691283ea3cce13
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-26 17:46:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,84 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
+client.on('message', message => {
+   if(message.content.startsWith(prefix + "دعوات")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} لديها ${inviteCount} تدعو.`);
+});
+  }
+});
 
+client.on('message',message =>{
+    if(message.content.startsWith(prefix + 'topinvites')) {
+     
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+ 
+ 
+ 
+ 
+client.on('message', message => {
+if(message.content === prefix + 'invite-info') {
+        let oi = message.mentions.users.first() ? message.mentions.users.first().id : message.author.id;
+        let Tag = message.mentions.users.first() ? message.mentions.users.first().tag : message.author.tag;
+        let Username = message.mentions.users.first() ? message.mentions.users.first().username : message.author.username;
+        let Avatar = message.mentions.users.first() ? message.mentions.users.first().avatarURL : message.author.avatarURL;
+       
+        message.guild.fetchInvites().then(invs => {
+            let member = client.guilds.get(message.guild.id).members.get(oi);
+            let personalInvites = invs.filter(i => i.inviter.id === oi);
+            let urll = invs.filter(i => i.inviter.id === oi);
+            let link = urll.reduce((p , v) => v.url +` , Total de membros recrutados no convite: ${v.uses}.\n`+ p, `\nServidor: ${message.guild.name} \n `);
+            let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+            let inviteCode = personalInvites.reduce((p, v) => v.code);
+            let possibleInvites = [['Total de membros recrutados:']];
+            possibleInvites.push([inviteCount, inviteCode]);
+            let user = message.mentions.users.first() || message.author;
+            let mem = message.guild.member(user);
+            let millisJoined = new Date().getTime() - mem.joinedAt.getTime();
+            let daysJoined = millisJoined / 1000 / 60 / 60 / 24;
+           
+            var inviteInfo = new Discord.RichEmbed()
+            .setTitle(`:incoming_envelope: **[INVITE INFO]** ${Username}`)
+            .setThumbnail(client.user.avatarURL)
+            .addField('**الدعوات**', `** ↝** [ شخص **${Number(inviteCount)}** ]`)
+            .addField('**تم الانضمام للسيرفر من**', `** ↝** [ يوم **${daysJoined.toFixed(0)}** ]`)
+            .addField('**رابط دعوة الانضمام**', `** ↝** [ **${inviteCode || 'Zm2U6we'}** ]`)
+            .setColor('#36393e')
+            .setTimestamp()
+            .setFooter(Tag, Avatar)
+           
+            message.channel.send(inviteInfo);
+    })
+           
+                                     
+                                     
+                                      };
+});
 
 client.login(process.env.BOT_TOKEN)
