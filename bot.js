7f91218016d95e203c1b44c5ad005fd67c4da7f8
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-26 18:00:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,32 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
-client.on('message', message => {
-   if(message.content.startsWith(prefix + "دعوات")) {
-    message.guild.fetchInvites().then(invs => {
-      let user = message.mentions.users.first() || message.author
-      let personalInvites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-message.channel.send(`${user} لديها ${inviteCount} تدعو.`);
-});
-  }
-});
+client.on('message',message =>{
+    if(message.content.startsWith(prefix + 'topinvites')) {
+     
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
 
- 
 
 client.login(process.env.BOT_TOKEN)
