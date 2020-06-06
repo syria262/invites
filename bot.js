2b9e096c64310576d263e84cbec33c25ccf6e7c0
syria262 <53955666+syria262@users.noreply.github.com>
2020-06-06 05:00:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,43 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require("discord.js");
+const { prefix, devs } = require("./config");
+const client = new Client({ disableEveryone: true });
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const convert = require("hh-mm-ss");
+const fetchVideoInfo = require("youtube-info");
+const botversion = require("./package.json").version;
+const simpleytapi = require("simple-youtube-api");
+const moment = require("moment");
+const fs = require("fs");
+const util = require("util");
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require("snekfetch");
+const guild = require("guild");
+const dateFormat = require("dateformat");
+const YouTube = require("simple-youtube-api");
+const youtube = new YouTube("AIzaSyAXaeBh837k38o_lwSADet8UTO7X21DGsY"); //تعديل اساسي سوي اي بي اي جديد
+const hastebins = require("hastebin-gen");
+const getYoutubeID = require("get-youtube-id");
+const yt_api_key = "AIzaSyAXaeBh837k38o_lwSADet8UTO7X21DGsY"; ///تعديل اساسي سوي اي بي اي جديد
+const pretty = require("pretty-ms");
+const queue = new Map();
+var table = require("table").table;
+const Discord = require("discord.js");
+const embedColor = "#36393e";
+const embedSuccess = "#22BF41";
+const embedFail = "#f30707";
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
 ///////////كود تكت
 client.on("message", message => {
   let tchannels = [];
